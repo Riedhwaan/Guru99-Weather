b77ca79c83439e3acde525561d7ba7e79db7db72
Riedhwaan <99826417+Riedhwaan@users.noreply.github.com>
2022-04-12 20:40:48 +0200

    Add files via upload

---
+++ Guru99/Guru99/Guru99Demo.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Opera;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99Demo
+{
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("C:\\3rdparty\\chrome");
+
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "http://https://www.accuweather.com/en/za/cape-town/306633/weather-forecast/306633";
+
+            string text = driver.FindElement(By.ClassName("div.temp-container")).Text;
+            
+        
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+     
+    }
+        
+}
